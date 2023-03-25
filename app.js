f6a5b74826bd204f3f4154466c66353b1a776c78
vishalyadav0507 <vishal364364yadav>
2023-03-25 12:51:58 +0530

    CRUD operation

--- app.js
+++ app.js
@@ -2,36 +2,36 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors=require("cors")
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
-
-app.use(
-  cors({
-  origin:"*"
-})
-)
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  console.log(req)
-  next()
+  User.findById('5baa2528563f16379fc8a610')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=> {
+mongoConnect(() => {
   app.listen(3000);
 });
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const adminController = require('../controllers/admin');
@@ -8,6 +6,14 @@ const router = express.Router();
 
 router.get('/add-product', adminController.getAddProduct);
 
+router.get('/products', adminController.getProducts);
+
 router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
